598e2e0a1f8d9a73c95b23917bd7a3bd26d67955
denisZlobich <sgsdgk9232@gmail.com>
2021-07-25 19:01:38 +0300

    разбил код на компоненты и подключил модуль

--- bundle.js
+++ bundle.js
@@ -1,95 +1,17 @@
 /******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!*********************!*\
-  !*** ./src/main.js ***!
-  \*********************/
-const tripInfo = document.querySelector('.trip-main__trip-info');
-const tripNav = document.querySelector('.trip-controls__trip-tabs');
-const tripControls = document.querySelector('.trip-main__trip-controls');
-const tripEvents = document.querySelector('.trip-events');
-const tripDays = document.querySelector('.trip-days');
-//Functions
-const createMenu = () => { 
-    return `
-        <nav class="trip-controls__trip-tabs  trip-tabs">
-            <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Table</a>
-            <a class="trip-tabs__btn" href="#">Stats</a>
-        </nav>
-    `
-};
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
 
-const createTripInfo = () => {
-    return `
-        <div class="trip-info__main">
-            <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
+/***/ "./src/components/form.js":
+/*!********************************!*\
+  !*** ./src/components/form.js ***!
+  \********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-            <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
-        </div>
-
-        <p class="trip-info__cost">
-            Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
-        </p>
-    `
-};
-
-const createTripFilters = () => {
-    return `
-        <form class="trip-filters" action="#" method="get">
-            <div class="trip-filters__filter">
-                <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
-                <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
-            </div>
-
-            <div class="trip-filters__filter">
-                <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
-                <label class="trip-filters__filter-label" for="filter-future">Future</label>
-            </div>
-
-            <div class="trip-filters__filter">
-                <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
-                <label class="trip-filters__filter-label" for="filter-past">Past</label>
-            </div>
-
-            <button class="visually-hidden" type="submit">Accept filter</button>
-    </form>
-    `
-};
-
-const createSort = () => {
-    return `
-        <form class="trip-events__trip-sort  trip-sort" action="#" method="get">
-            <span class="trip-sort__item  trip-sort__item--day">Day</span>
-
-            <div class="trip-sort__item  trip-sort__item--event">
-                <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event" checked>
-                <label class="trip-sort__btn" for="sort-event">Event</label>
-            </div>
-
-            <div class="trip-sort__item  trip-sort__item--time">
-                <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
-                <label class="trip-sort__btn" for="sort-time">
-                    Time
-                    <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
-                    <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
-                    </svg>
-                </label>
-            </div>
-
-            <div class="trip-sort__item  trip-sort__item--price">
-                <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
-                <label class="trip-sort__btn" for="sort-price">
-                    Price
-                    <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
-                    <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
-                    </svg>
-                </label>
-            </div>
-
-            <span class="trip-sort__item  trip-sort__item--offers">Offers</span>
-        </form>
-    `
-};
-
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "createForm": () => (/* binding */ createForm)
+/* harmony export */ });
 const createForm = () => {
     return `
     <form class="trip-events__item  event  event--edit" action="#" method="post">
@@ -254,6 +176,92 @@ const createForm = () => {
     `
 };
 
+
+
+/***/ }),
+
+/***/ "./src/components/menu.js":
+/*!********************************!*\
+  !*** ./src/components/menu.js ***!
+  \********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "createMenu": () => (/* binding */ createMenu)
+/* harmony export */ });
+const createMenu = () => { 
+    return `
+        <nav class="trip-controls__trip-tabs  trip-tabs">
+            <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Table</a>
+            <a class="trip-tabs__btn" href="#">Stats</a>
+        </nav>
+    `
+};
+
+
+
+/***/ }),
+
+/***/ "./src/components/sort.js":
+/*!********************************!*\
+  !*** ./src/components/sort.js ***!
+  \********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "createSort": () => (/* binding */ createSort)
+/* harmony export */ });
+const createSort = () => {
+    return `
+        <form class="trip-events__trip-sort  trip-sort" action="#" method="get">
+            <span class="trip-sort__item  trip-sort__item--day">Day</span>
+
+            <div class="trip-sort__item  trip-sort__item--event">
+                <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event" checked>
+                <label class="trip-sort__btn" for="sort-event">Event</label>
+            </div>
+
+            <div class="trip-sort__item  trip-sort__item--time">
+                <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
+                <label class="trip-sort__btn" for="sort-time">
+                    Time
+                    <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
+                    <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
+                    </svg>
+                </label>
+            </div>
+
+            <div class="trip-sort__item  trip-sort__item--price">
+                <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
+                <label class="trip-sort__btn" for="sort-price">
+                    Price
+                    <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
+                    <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
+                    </svg>
+                </label>
+            </div>
+
+            <span class="trip-sort__item  trip-sort__item--offers">Offers</span>
+        </form>
+    `
+};
+
+
+
+/***/ }),
+
+/***/ "./src/components/tripDay.js":
+/*!***********************************!*\
+  !*** ./src/components/tripDay.js ***!
+  \***********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "createTripDay": () => (/* binding */ createTripDay)
+/* harmony export */ });
 const createTripDay = () => {
     return `
     <ul class="trip-days">
@@ -378,16 +386,169 @@ const createTripDay = () => {
 };
 
 
+
+/***/ }),
+
+/***/ "./src/components/tripFilters.js":
+/*!***************************************!*\
+  !*** ./src/components/tripFilters.js ***!
+  \***************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "createTripFilters": () => (/* binding */ createTripFilters)
+/* harmony export */ });
+const createTripFilters = () => {
+    return `
+        <form class="trip-filters" action="#" method="get">
+            <div class="trip-filters__filter">
+                <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
+                <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
+            </div>
+
+            <div class="trip-filters__filter">
+                <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
+                <label class="trip-filters__filter-label" for="filter-future">Future</label>
+            </div>
+
+            <div class="trip-filters__filter">
+                <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
+                <label class="trip-filters__filter-label" for="filter-past">Past</label>
+            </div>
+
+            <button class="visually-hidden" type="submit">Accept filter</button>
+    </form>
+    `
+};
+
+
+
+/***/ }),
+
+/***/ "./src/components/tripInfo.js":
+/*!************************************!*\
+  !*** ./src/components/tripInfo.js ***!
+  \************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "createTripInfo": () => (/* binding */ createTripInfo)
+/* harmony export */ });
+const createTripInfo = () => {
+    return `
+        <div class="trip-info__main">
+            <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
+
+            <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
+        </div>
+
+        <p class="trip-info__cost">
+            Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
+        </p>
+    `
+};
+
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+/*!*********************!*\
+  !*** ./src/main.js ***!
+  \*********************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _components_menu_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./components/menu.js */ "./src/components/menu.js");
+/* harmony import */ var _components_tripInfo_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./components/tripInfo.js */ "./src/components/tripInfo.js");
+/* harmony import */ var _components_tripFilters_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./components/tripFilters.js */ "./src/components/tripFilters.js");
+/* harmony import */ var _components_sort_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./components/sort.js */ "./src/components/sort.js");
+/* harmony import */ var _components_form_js__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./components/form.js */ "./src/components/form.js");
+/* harmony import */ var _components_tripDay_js__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./components/tripDay.js */ "./src/components/tripDay.js");
+
+
+
+
+
+
+
+const tripInfo = document.querySelector('.trip-main__trip-info');
+const tripNav = document.querySelector('.trip-controls__trip-tabs');
+const tripControls = document.querySelector('.trip-main__trip-controls');
+const tripEvents = document.querySelector('.trip-events');
+/* const tripDays = document.querySelector('.trip-days'); */
+
+
 const addElements = () => {
-    tripInfo.innerHTML += createTripInfo();
-    tripNav.innerHTML += createMenu();
-    tripControls.innerHTML += createTripFilters();
-    tripEvents.innerHTML += createSort();
-    tripEvents.innerHTML += createForm();
-    tripEvents.innerHTML += createTripDay();
+    tripInfo.innerHTML += (0,_components_tripInfo_js__WEBPACK_IMPORTED_MODULE_1__.createTripInfo)();
+    tripNav.innerHTML += (0,_components_menu_js__WEBPACK_IMPORTED_MODULE_0__.createMenu)();
+    tripControls.innerHTML += (0,_components_tripFilters_js__WEBPACK_IMPORTED_MODULE_2__.createTripFilters)();
+    tripEvents.innerHTML += (0,_components_sort_js__WEBPACK_IMPORTED_MODULE_3__.createSort)();
+    tripEvents.innerHTML += (0,_components_form_js__WEBPACK_IMPORTED_MODULE_4__.createForm)();
+    tripEvents.innerHTML += (0,_components_tripDay_js__WEBPACK_IMPORTED_MODULE_5__.createTripDay)();
 };
 //Events
 document.addEventListener('DOMContentLoaded', addElements);
+
+})();
 
 /******/ })()
 ;
