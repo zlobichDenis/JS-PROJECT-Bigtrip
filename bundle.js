ebc512db1257882118f901337d6a64da2d8b46fa
denisZlobich <sgsdgk9232@gmail.com>
2021-07-31 17:32:00 +0300

    3. Создал объекты с данными точек назначения и дополнительных опций для каждого типа транспорта

--- bundle.js
+++ bundle.js
@@ -262,6 +262,56 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "createTripDay": () => (/* binding */ createTripDay)
 /* harmony export */ });
+const pointOfDestination =  {
+  "date_from": "2019-07-10T22:55:56.845Z",
+  "date_to": "2019-07-11T11:22:13.375Z",
+  "destination": 'Paris', // Функция для рандомного определения элемента из массива
+  "id": "0", // Счетчик i
+  "is_favorite": Math.random() > 0.5,
+  "offers": null, // Элмент из массива offers
+};
+
+const createTripEventMarkup = () => {
+
+  return `
+  <ul class="trip-events__list">
+  <li class="trip-events__item">
+    <div class="event">
+      <div class="event__type">
+        <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
+      </div>
+      <h3 class="event__title">Taxi to Amsterdam</h3>
+
+      <div class="event__schedule">
+        <p class="event__time">
+          <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
+          &mdash;
+          <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
+        </p>
+        <p class="event__duration">30M</p>
+      </div>
+
+      <p class="event__price">
+        &euro;&nbsp;<span class="event__price-value">20</span>
+      </p>
+
+      <h4 class="visually-hidden">Offers:</h4>
+      <ul class="event__selected-offers">
+        <li class="event__offer">
+          <span class="event__offer-title">Order Uber</span>
+          &plus;
+          &euro;&nbsp;<span class="event__offer-price">20</span>
+         </li>
+      </ul>
+
+      <button class="event__rollup-btn" type="button">
+        <span class="visually-hidden">Open event</span>
+      </button>
+    </div>
+  </li>
+  `
+};
+
 const createTripDay = () => {
     return `
     <ul class="trip-days">
