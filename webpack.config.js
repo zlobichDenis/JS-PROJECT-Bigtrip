3586c88cb7d518eb24f086c327aa2eedd0b7f30c
denisZlobich <sgsdgk9232@gmail.com>
2021-08-18 14:52:24 +0300

    8.4 Установил библиотеки flatpickr, moment и дописал webpack-config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require(`path`);
+const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
 
 
 module.exports = {
@@ -8,5 +9,18 @@ module.exports = {
         filename: `bundle.js`,
         path: path.join(__dirname, `./public`),
     },
-    devtool: `source-map`
+    devtool: `source-map`,
+    plugins: [
+        new MomentLocalesPlugin({
+            localesToKeep: ['es-us'],
+        }),
+    ],
+    module:{
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
 };
